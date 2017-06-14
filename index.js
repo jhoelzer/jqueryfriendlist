907fef3307c64d10a66e40767eb7cda39554cba0
jhoelzer <hoelzer.jenny@gmail.com>
2017-06-14 13:49:51 -0400

    more index

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/public')),
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
 
-http.listen(3000, function() {
-	console.log('listening of port 3000');
-	});
+http.listen(process.env.PORT || 3000, function() {
+	console.log('Listening on *:3000');
 });
