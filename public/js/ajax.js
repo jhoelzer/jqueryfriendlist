294015c065b3b2fcd620502c4294355180051845
jhoelzer <hoelzer.jenny@gmail.com>
2017-06-20 15:04:52 -0400

    modded index, ajax, and css

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -104,3 +104,18 @@ $(document).ready(function() {
 		});
 	});
 });
+
+//back to top button
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
